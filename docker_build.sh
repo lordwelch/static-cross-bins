650e2d4d2b0202af8628335a82180816a07e43f1
Timmy Welch <timmy@narnian.us>
2023-05-05 18:39:07 -0700

    Create directories before running docker

--- docker_build.sh
+++ docker_build.sh
@@ -7,7 +7,7 @@ set -e
 if [ -e .docker_context ]; then
 	rm -r .docker_context/*
 fi
-mkdir -p .docker_context
+mkdir -p .docker_context output sources sysroot
 cp -r Dockerfile Makefile include .docker_context
 docker build -t static-builder .docker_context
 docker run -it -v "${PWD}/output":"/build/output:Z" -v "${PWD}/sources":"/build/sources:Z" --rm static-builder "$@"
